e9a363af28dc31fd74f2a1ae4f5bb1ad3e5881ef
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2026-08-18 17:31:29 -0500

    Implement processing transactions

---
+++ web/src/routes/api/v1/server/currency/process/[transactionId]/+server.ts
@@ -0,0 +1,64 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { SERVER_TOKEN } from '$env/static/private';
+import { getComputerByClientID, getCurrencyByTransactionId } from '$lib/server/airtable';
+
+
+export const POST: RequestHandler = async ({ params, request }) => {
+    const {
+        clientId
+    }: {
+        clientId: string;
+    } = await request.json();
+    const transactionId = params.transactionId;
+
+    // Ensure valid token
+    const token = request.headers.get('Authorization');
+    if (token !== `Bearer ${SERVER_TOKEN}`) {
+        return json({ error: 'Invalid token' }, { status: 401 });
+    }
+    
+    // Get the transaction
+    const transaction = await getCurrencyByTransactionId(transactionId)
+    if (!transaction) {
+        return json({ error: 'Transaction not found' }, { status: 404 });
+    }
+
+    // TODO: Fix typing
+
+    /* These conditions are already check by processCurrency
+    if (transaction['Needs Auth'] && !transaction['Authorized']) {
+        return json({ error: 'Transaction needs to be authorized before it can be processed' }, { status: 400 });
+    }
+
+    if (transaction['Processed']) {
+        return json({ error: 'Transaction has already been processed' }, { status: 400 });
+    }
+    */
+
+    // Fetch the user's computer
+    const computer = await getComputerByClientID(clientId);
+
+    if (!computer) {
+        return json({ error: 'Computer not found' }, { status: 404 });
+    }
+
+    // TODO: Fix typing
+    if (transaction['Slack ID (from To)'] !== computer.fields["Slack ID (from Owner)"]) {
+        return json({ error: 'Computer does not belong to the receiving user' }, { status: 403 });
+    }
+
+    // Process the transaction
+    const result = await processCurrency({
+        transactionId: transaction['Transaction ID'],
+        note: transaction['Note'],
+        fromId: transaction['From'],
+        toId: transaction['To'],
+        amount: transaction['Amount'],
+        needsAuth: transaction['Needs Auth'],
+        authorized: transaction['Authorized'],
+        processed: transaction['Processed']
+    });
+    
+    return json(result);
+};
